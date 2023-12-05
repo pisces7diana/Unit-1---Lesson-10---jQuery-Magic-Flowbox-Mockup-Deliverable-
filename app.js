5668658a7c3021b55cf0554894651cb09766ffdf
pisces7diana <email7diana@gmail.com>
2023-12-04 21:07:08 -0800

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -7,8 +7,29 @@ $(() => {
   console.log($container);
 
   const $h1 = $('<h1>');
-
+  console.log($h1)
   $h1.text('Hogwarts');
   $container.append($h1);
-  console.log($h1)
+
+  const $h2 = $('<h2>');
+  console.log($h2)
+  $h2.text('Diana');
+  $container.append($h2);
+
+  const $h3 = $('<h3>');
+  console.log($h3)
+  $h3.text('Ravenclaw');
+  $container.append($h3);
+
+  const $h4 = $('<h4>');
+  console.log($h4)
+  $h4.addClass('cat').text('Pennygirl and Roam');
+  $container.append($h4);
+
+  const $h5 = $('<h4>');
+  console.log($h5)
+  $h5.text('Holly Wand with Unicorn Hair Core');
+  $container.append($h5);
+
+
 });
